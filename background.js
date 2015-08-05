e84e21dc7cadc2fe7989a288684377ac2c91b255
Nataraj <atchula.nataraj@live.in>
2015-08-05 20:54:49 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Nattu - App - Please stand once', {
+      icon: 'images/love-128.png',
+      body: "continously sitting in chair is not halthy way to work. now Its time to stand for atleast half minute!",
+    });
+
+    notification.onclick = function () {
+            
+    };
+
+  }
+}
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    if (alarm.name == 'name-of-alarm') {
+        // Do something...
+        notifyMe();
+    }
+});
+
+// Create the alarm:
+chrome.alarms.create('name-of-alarm', {
+    periodInMinutes: 60
+});
